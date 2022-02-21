bc0ba634f2ecff826c8f8896b98a34d60dfeeceb
hihi-hehe <gothy0117@gmail.com>
2022-02-21 18:21:21 +0900

    completed

--- Q1.py
+++ Q1.py
@@ -1,2 +1,12 @@
-for i in range(5):
-    print(i)
+def solution(N, fearLevel):
+    answer = 0
+    nGroup={}
+    for n in fearLevel:
+        nGroup[n]=(nGroup.get(n, 0)) + 1
+    for key,_ in nGroup.items():
+        nGroup[key]=(nGroup.get(key)) // key
+        answer=answer+nGroup[key]
+    return answer
+
+if __name__ == "__main__":
+    solution(5, [2,3,1,2,2])
